c3c91b15040601ac552df11863cc10745016be1b
elduarduz <54306639+elduarduz@users.noreply.github.com>
2020-05-26 23:55:10 +0700

    Home Actovoty open fragment progress

--- app/src/main/java/com/example/ticketbioskop/HomeActivity.java
+++ app/src/main/java/com/example/ticketbioskop/HomeActivity.java
@@ -1,20 +1,48 @@
 package com.example.ticketbioskop;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import android.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends Activity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_fragment);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
 
-    public void profil(View view){
-        startActivity(new Intent(getApplicationContext(), userprofile.class));
-        finish();
-    }
+    private  BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_poin:
+                            break;
+                        case R.id.navigation_pembayaran:
+                            break;
+                    }
+                    getFragmentManager().beginTransaction().replace(R.id.fl_fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
--- app/src/main/java/com/example/ticketbioskop/HomeFragment.java
+++ app/src/main/java/com/example/ticketbioskop/HomeFragment.java
@@ -1,18 +1,28 @@
 package com.example.ticketbioskop;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+import android.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.ticketbioskop.R;
 
 public class HomeFragment extends Fragment {
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         return root;
+    }
+
+    public void profil(View view){
+
     }
 }
