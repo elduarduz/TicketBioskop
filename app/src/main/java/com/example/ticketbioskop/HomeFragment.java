e0edefa33cd9d597ce8fb0fb8c5e2dff729728bc
williamjspt <jancookqwe@gmail.com>
2020-05-28 22:12:44 +0700

    booking sistem

---
+++ app/src/main/java/com/example/ticketbioskop/HomeFragment.java
@@ -0,0 +1,63 @@
+package com.example.ticketbioskop;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.ticketbioskop.menuFragment.MainFragment;
+import com.example.ticketbioskop.menuFragment.Point;
+import com.example.ticketbioskop.menuFragment.ProfileFragment;
+import com.example.ticketbioskop.menuFragment.Transaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        BottomNavigationView bottomNavigationView = v.findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        switchFragment(new MainFragment());
+                        return true;
+                    case R.id.menu_calculate_calory:
+                        switchFragment(new Point());
+                        return true;
+                    case R.id.menu_consume_calory:
+                        switchFragment(new Transaction());
+                        return true;
+                    case R.id.menu_list_calory:
+                        switchFragment(new ProfileFragment());
+                        return true;
+                }
+                return false;
+            }
+        });
+        switchFragment(new MainFragment());
+        return v;
+    }
+    private void switchFragment(Fragment newFragment) {
+        FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fl_fragment_container, newFragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/ticketbioskop/menuFragment/ProfileFragment.java
@@ -0,0 +1,47 @@
+package com.example.ticketbioskop.menuFragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.ticketbioskop.LoginActivity;
+import com.example.ticketbioskop.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v  = inflater.inflate(R.layout.fragment_profile, container, false);
+        Button LogoutButton = v.findViewById(R.id.btn_logout);
+        LogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+        return v;
+    }
+
+    public void logout(){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getActivity(), LoginActivity.class));
+        getActivity().finishAffinity();
+    }
+}
