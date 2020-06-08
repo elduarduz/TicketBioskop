b3acf7b17767488f4cc83f9d5ad129c3e0bf55a9
tes <albie.ferdinata@ti.ukdw.ac.id>
2020-06-08 21:43:12 +0700

    read email profile

--- app/src/main/java/com/example/ticketbioskop/menuFragment/ProfileFragment.java
+++ app/src/main/java/com/example/ticketbioskop/menuFragment/ProfileFragment.java
@@ -10,10 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ticketbioskop.LoginActivity;
 import com.example.ticketbioskop.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
@@ -22,6 +24,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class ProfileFragment extends Fragment {
 
     private Fragment newFragment;
+    FirebaseAuth firebaseAuth;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -35,10 +38,15 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View v  = inflater.inflate(R.layout.fragment_profile, container, false);
         Button LogoutButton = v.findViewById(R.id.btn_logout);
-
+        TextView displayNama = v.findViewById(R.id.nama);
         Button editProfile = v.findViewById(R.id.btn_edit);
+        firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         Button topUpbutton = v.findViewById(R.id.btn_topup);
+
+
+        displayNama.setText(user.getEmail());
 
         topUpbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +62,7 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+
         LogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,6 +71,7 @@ public class ProfileFragment extends Fragment {
         });
         return v;
     }
+
 
     public void topap(){
         startActivity(new Intent(getActivity(), Topup.class));
